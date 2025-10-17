884132d04199b2abc841346e87e2b92ba02a6d15
KING156454 <alex.133.b@gmail.com>
2025-10-17 11:12:40 +0200

    e

---
+++ travail.py
@@ -0,0 +1,29 @@
+class Arbre() :
+    def __init__(self, nom_racine) :
+        self.nom_racine = nom_racine
+        self.liste_fils = []
+    
+    def ajoute_fils(self, fils) :
+        self.liste_fils.append(fils)
+    
+    def hauteur(self) :
+        pass
+
+    def taille(self) :
+        pass
+
+    def binaire(self) :
+        pass
+    
+    def parcours_largeur (self) :
+        pass
+
+    def parcours_profondeur_prefixe(self) :
+        pass
+
+    def liste_aretes(self) :
+        pass
+
+    def affiche(self) :
+        pass
+    
